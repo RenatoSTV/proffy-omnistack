cb8d9e6e56b60eb9e1a9102d771d619cb060a94d
Renato ST <renatostv@gmail.com>
2020-08-10 07:59:04 -0300

    proffy omnistack
    - Criação do site Poffy versões web e mobile.
    
    * Reupload do projeto pro problemas com o repositório antigo.

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './assets/styles/global.css'
+import Routes from './routs';
+
+// JSX - JavaScript + XML
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
